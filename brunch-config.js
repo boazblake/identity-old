5c1fe13d5aca2400a6817f2bcd846aebe440383f
Boaz Blake <BBlake@goempyrean.com>
2021-04-10 13:26:23 -0500

    workbox

--- brunch-config.js
+++ brunch-config.js
@@ -65,22 +65,10 @@ exports.plugins = {
     verbose: true, //shows each file that is copied to the destination directory
     onlyChanged: true, //only copy a file if it's modified time has changed (only effective when using brunch watch)
   },
-  swPrecache: {
-    swFileName: "service-worker.js",
-    options: {
-      autorequire: [
-        "app/assets/index.html",
-        "app/assets/images",
-        "app/assets/files",
-      ],
-      staticFileGlobs: [
-        "docs/app.css",
-        "docs/app.js",
-        "docs/vendor.js",
-        "docs/index.html",
-      ],
-      stripPrefix: "docs/",
-    },
+  workbox: {
+    globDirectory: "docs/",
+    globPatterns: ["**/*.*"],
+    swDest: "docs/sw.js",
   },
   "@babel": { presets: ["env"] },
   terser: {
